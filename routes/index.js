bd566c3138328c334f9b453b20b36f7f3cc9ab4e
Jarod Reyes <jarodreyes@gmail.com>
2017-02-14 06:50:35 -0800

    Added new homepage.

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Help Me Shakespeare!' });
+  res.sendFile('index.html');
 });
 
 module.exports = router;
